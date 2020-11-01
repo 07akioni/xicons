0da25d496cf675695d355335cdbc977e88d83f13
07akioni <07akioni2@gmail.com>
2020-11-01 15:51:13 +0800

    build: vercel build script

--- init.sh
+++ init.sh
@@ -2,5 +2,5 @@ mkdir resources
 git clone https://github.com/microsoft/fluentui-system-icons.git resources/fluent
 git clone https://github.com/ionic-team/ionicons.git resources/ionicons-v5
 git clone https://github.com/ionic-team/ionicons.git resources/ionicons-v4
-cd resources/ion-v4
+cd resources/ionicons-v4
 git checkout v4.6.4-1
