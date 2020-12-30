43c0572a084af1df5ef507f3c15c006f78de65dc
07akioni <07akioni2@gmail.com>
2020-12-30 19:10:30 +0800

    refactor: release script

--- scripts/release.js
+++ scripts/release.js
@@ -1,50 +1,91 @@
 const path = require('path')
-const fs = require('fs')
+const fse = require('fs-extra')
 const execa = require('execa')
 const config = require('./config')
-const { name, version, repository } = require('../package.json')
+const { version, repository } = require('../package.json')
+
+const frameworks = [
+  {
+    dir: 'react',
+    scope: 'ricons',
+    descriptionPrefix: 'React SVG icon components',
+    keywords: [
+      'react',
+      'svg'
+    ]
+  },
+  {
+    dir: 'vue3',
+    scope: 'vicons',
+    descriptionPrefix: 'Vue3 SVG icon components',
+    keywords: [
+      'vue-next',
+      'vue',
+      'svg'
+    ]
+  },
+  {
+    dir: 'vue2',
+    scope: 'v2icons',
+    descriptionPrefix: 'Vue2 SVG icon components',
+    keywords: [
+      'vue',
+      'svg'
+    ]
+  },
+  {
+    dir: 'SVG',
+    scope: 'sicons',
+    descriptionPrefix: 'SVG icons',
+    keywords: [
+      'svg'
+    ]
+  }
+]
 
 const metaTemplate = {
   "keywords": [
-    "vue",
-    "svg",
     "icon"
-  ],
-  "files": [
-    "react",
-    "vue2",
-    "vue3",
-    "svg"
   ],
   "author": "07akioni",
   "license": "MIT",
   "dependencies": {},
-  "sideEffects": false
+  "sideEffects": false,
+  "module": "index.js"
 }
 
 ;(async () => {
   for (const iconSetConfig of config) {
-    const scopedPackageName = `@${name}/${iconSetConfig.name}`
-    const scopedPackagePath = path.resolve(__dirname, '..', 'dist', iconSetConfig.name)
-    const meta = JSON.stringify({
-      name: scopedPackageName,
-      version,
-      description: iconSetConfig.description,
-      repository,
-      ...metaTemplate
-    }, 0, 2)
-    fs.writeFileSync(path.resolve(scopedPackagePath, 'package.json'), meta)
-    console.log('release:', scopedPackageName)
-    try {
-      const result = await execa('npm', [
-        'publish',
-        '--access=public'
-      ], {
-        cwd: scopedPackagePath
-      })
-      console.log(result.stdout)
-    } catch (error) {
-      console.log(error)
+    for (const { dir: frameworkDir, scope, descriptionPrefix, keywords } of frameworks) {
+      const scopedPackageName = `@${scope}/${iconSetConfig.name}`
+      const scopedPackagePath = path.resolve(__dirname, '..', 'dist', iconSetConfig.name, frameworkDir)
+      const description = iconSetConfig.description(descriptionPrefix)
+      const meta = JSON.stringify({
+        name: scopedPackageName,
+        version,
+        description,
+        repository,
+        ...metaTemplate,
+        keywords: metaTemplate.keywords.concat(keywords).concat(iconSetConfig.keywords)
+      }, 0, 2)
+      await fse.writeFile(path.resolve(scopedPackagePath, 'package.json'), meta)
+      await fse.writeFile(path.resolve(scopedPackagePath, 'README.md'), 
+        `# ${scopedPackageName}\n\n` +
+        description + '\n\n' +
+        'A part of [`xicons`](https://github.com/07akioni/xicons) project.'
+      )
+      console.log('release:', scopedPackageName)
+      try {
+        const result = await execa('npm', [
+          'publish',
+          '--access=public'
+        ], {
+          cwd: scopedPackagePath
+        })
+        console.log(result.stdout)
+      } catch (error) {
+        console.log(error)
+      }
     }
   }
 })()
