f46638696a791d5dce17ba293da8425def075471
07akioni <07akioni2@gmail.com>
2021-03-01 14:13:56 +0800

    build: move to lerna

---
+++ packages/ricons-utils/src/Icon.tsx
@@ -0,0 +1,36 @@
+import * as React from 'react'
+import { useEffect, useMemo, useContext } from 'react'
+import { mountStyle } from '@xicons/utils'
+import type { IconConfigProviderContext } from './interface'
+import { iconConfigProviderContext } from './IconConfigProvider'
+import { defaultTag } from './config'
+
+interface IconProps extends IconConfigProviderContext {
+  children?: React.ReactNode
+}
+
+export function Icon ({ size, color, tag = 'span', children }: IconProps) {
+  const iconConfig = useContext(iconConfigProviderContext)
+  const mergedSize = useMemo(() => {
+    const _size = size ?? iconConfig?.size
+    if (_size === undefined) return undefined
+    if (typeof _size === 'number' || /^\d+$/.test(_size)) return `${_size}px`
+    return _size
+  }, [size, iconConfig?.size])
+  const mergedColor = color ?? iconConfig?.color
+  const MergedTag = tag ?? iconConfig?.tag ?? defaultTag
+  useEffect(() => {
+    mountStyle()
+  }, [])
+  return (
+    <MergedTag 
+      className="xicons"
+      style={{
+        color: mergedColor,
+        fontSize: mergedSize
+      }}
+    >
+      {children}
+    </MergedTag>
+  )
+}
